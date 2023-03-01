3b4562a30ab13d21261870068a4b0e49b7764e66
aliahmadjan <i190572@nu.edu.pk>
2023-03-01 19:25:18 +0500

    Deployment Testing

--- frontend/src/components/AdminSidebar.jsx
+++ frontend/src/components/AdminSidebar.jsx
@@ -50,7 +50,7 @@ export default function AdminSidebar({navSize, changeNavSize}) {
         }
       let logintoken = localStorage.getItem("logtoken")
       axios.defaults.headers.common["Authorization"] = `Bearer ${logintoken}`;
-      axios.get("https://main--reliable-biscuit-f62ccb.netlify.app/admin/viewprofile")
+      axios.get("https://sea-lion-app-7u3ed.ondigitalocean.app/admin/viewprofile")
         .then(res=> {
                 //console.log(res.data)
                 setUserID(res.data._id);
