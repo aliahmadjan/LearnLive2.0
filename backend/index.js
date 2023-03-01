d6b363fc5cfbf2cf9c79fe70f5b75fab7897bea6
aliahmadjan <i190572@nu.edu.pk>
2023-03-01 18:52:19 +0500

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,9 @@ const port = process.env.PORT || 5000;
 //const port= process.env.BASE_URL
 //const port = "https://main--reliable-biscuit-f62ccb.netlify.app";
 
-//app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://learnlive-testing.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://learnlive-testing-7u6al.ondigitalocean.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
