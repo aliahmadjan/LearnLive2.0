353535dd1cddfe00d27a8e3956b3bb6332180024
aliahmadjan <i190572@nu.edu.pk>
2023-03-02 21:42:58 +0500

    Testing

--- frontend/src/components/AdminSidebar.jsx
+++ frontend/src/components/AdminSidebar.jsx
@@ -50,7 +50,7 @@ export default function AdminSidebar({navSize, changeNavSize}) {
         }
       let logintoken = localStorage.getItem("logtoken")
       axios.defaults.headers.common["Authorization"] = `Bearer ${logintoken}`;
-      axios.get("http://localhost:10000/admin/viewprofile")
+      axios.get("https://learnlive.onrender.com/admin/viewprofile")
         .then(res=> {
                 //console.log(res.data)
                 setUserID(res.data._id);
