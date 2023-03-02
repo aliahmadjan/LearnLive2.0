42f1c28f85db10c67fd29d22d44e3432e908e057
aliahmadjan <i190572@nu.edu.pk>
2023-03-02 21:36:07 +0500

    Testing

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -21,7 +21,7 @@ const AdminLogin = () => {
     {
         e.preventDefault();
         try {
-           const res=  await axios.post('http://localhost:10000/admin/verifylogin', {
+           const res=  await axios.post('https://learnlive.onrender.com/admin/verifylogin', {
                 email: email,
                 password: password,
             
