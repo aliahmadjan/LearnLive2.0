b7f1e2a789a47e23287e797f20de0c200a29e2dc
ihaseeb8 <ihaseebullah.8@gmail.com>
2023-06-12 15:51:00 +0500

    Added a chatbot

---
+++ frontend/src/components/Chat/Chat.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { Box, Input, Button, Flex, Text, IconButton } from '@chakra-ui/react';
+
+const query = async (data) => {
+  const response = await fetch(
+    'https://api-inference.huggingface.co/models/deepset/roberta-base-squad2',
+    {
+      headers: { Authorization: 'Bearer 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' },
+      method: 'POST',
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+};
+
+const Chat = () => {
+  const [messages, setMessages] = useState([{ sender: 'bot', message: 'Hi, How can i help you today?' }]);
+  const [inputValue, setInputValue] = useState('');
+  const [context, setContext] = useState("We are WEEPRO CODERS. We teach early year learners coding. We are offering fun-filled and project-based camps of Scratch, Python, HTML, MIT App Inventor, Unity and CS unplugged (Coding without Code) to learners between 5-18 years. \
+  Website : https://web.facebook.com/weeProCoders \
+  Specialties: Problem Solving, Logic building, Computational Thinking, Creativity, Learning through Gamification, Learning is fun, Playful and engaging classroom experience, Future Skills, Educational Games, Cognition, Abstraction, Learning trajectory, Active classrooms, Adaptive learning, Bridging gap between theory and practice, Brain train, and Coding")
+
+  const handleSendMessage = async () => {
+    if (inputValue.trim() !== '') {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { sender: 'user', message: inputValue },
+      ]);
+      setInputValue('');
+  
+      const response = await query({
+        inputs: {
+          question: inputValue,
+          context: context,
+        },
+      });
+  
+      console.log(response);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { sender: 'bot', message: response.answer },
+      ]);
+    }
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <Box
+      w="400px"
+      p={1}
+      border="2px solid orange"
+      borderRadius={10}
+      backgroundImage={'linear-gradient(to bottom, #fddb92 0%, #d1fdff 100%);'}
+    >
+      <Box 
+        p={2}
+        overflowY="scroll" 
+        minHeight="300px" 
+        maxHeight='400px'
+        display={'flex'}
+        flexDir={'column'}
+        sx={{
+            '&::-webkit-scrollbar': {
+              width: '8px',
+              borderRadius: '8px',
+              backgroundColor: 'white',
+            },
+            '&::-webkit-scrollbar-thumb': {
+              backgroundColor: `orange.500`,
+              borderRadius: '8px',
+            },
+          }}
+        >   
+        {messages.map((message, index) => (
+          <Box
+            key={index}
+            mb="10px"
+            p="5px"
+            borderRadius="5px"
+            maxW="80%"
+            alignSelf={message.sender === 'user' ? 'flex-end' : 'flex-start'}
+            bg={message.sender === 'user' ? 'orange.400' : 'white'}
+            color={message.sender === 'user' ? '#fff' : '#000'}
+          >
+            {message.message}
+          </Box>
+        ))}
+      </Box>
+
+      <Flex mt="10px">
+        <Input
+          type="text"
+          value={inputValue}
+          bg='white'
+          focusBorderColor='orange.500'
+          
+          placeholder='Type your question'
+          onChange={handleChange}
+          mr={2}
+        />
+        <IconButton
+            icon={<i class="fa-solid fa-paper-plane"></i>}
+            size="md"
+            variant="outline"
+            aria-label="Chatbot"
+            colorScheme="orange"
+            onClick={handleSendMessage}
+          />
+
+      </Flex>
+    </Box>
+  );
+};
+
+export default Chat;
+
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,21 +1,50 @@
-import { Button, Container } from '@chakra-ui/react'
+import { Button, Container, Box, IconButton } from '@chakra-ui/react'
 import React from 'react'
 import LogInNavBar from '../components/LogInNavBar'
 import StudentLogin from '../components/StudentLogin'
 import TeacherLogin from '../components/TeacherLogin'
 import AdminLogin from '../components/AdminLogin'
+import Chat from '../components/Chat/Chat'
 import { useState } from 'react'
 import useStore from '../store'
 
 const LoginPage = () => {
 
+  const [showBox, setShowBox] = useState(false);
   const user = useStore(state => state.user)
+
+  const handleButtonClick = () => {
+    setShowBox(!showBox);
+  };
 
   return (
     <Container maxW="100%" h="100vh" p={0} bg="black">
         <LogInNavBar></LogInNavBar>
         {user == "student" ? <StudentLogin ></StudentLogin> : 
           ( user == "teacher" ? <TeacherLogin ></TeacherLogin> : <AdminLogin></AdminLogin>)}
+
+          <IconButton
+            position="fixed"
+            bottom={12}
+            left={12}
+            icon={<i class="fa-solid fa-message"></i>}
+            size="lg"
+            variant="outline"
+            aria-label="Chatbot"
+            colorScheme="orange"
+            onClick={handleButtonClick}
+          />
+
+        {showBox && (
+          <Box position="fixed"
+            bottom={24}
+            my={1}
+            left={12}>
+            <Chat />
+
+          </Box>
+        )}
+        
     </Container>
   )
 }
